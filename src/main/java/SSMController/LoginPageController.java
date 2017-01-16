8fdf7c28a70666190dac379d06026416f011b8c5
Swathi <Swathi@Swathi-PC>
2017-01-16 22:44:19 +0530

    Code commited by jalakam Swathi

---
+++ src/main/java/SSMController/LoginPageController.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package SSMController;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import Factory.DataProviderFactory;
+import SSMModel.LoginPageModel;
+import SSMTestCases.BaseClass;
+
+/**
+ * @author SMuppidi
+ *
+ */
+public class LoginPageController extends BaseClass
+{
+		WebDriver driver;
+			   
+		public static LoginPageModel SSMloginPage;
+
+		public LoginPageController(WebDriver driver)
+		{
+			this.driver = driver;
+			SSMloginPage = new SSMModel.LoginPageModel(driver);
+		}
+		
+	
+		
+		public void SSMWebLogin(String uname, String pwd) throws InterruptedException
+		{
+			
+			SSMloginPage.setSSMUserName(uname);
+			test.log(LogStatus.INFO,"  User name entered successfully");
+			SSMloginPage.setSSMPasswd(pwd);
+			test.log(LogStatus.INFO,"Password entered successfully");
+			SSMloginPage.clickLogin();
+			test.log(LogStatus.INFO,"Login button is clicked");
+			
+		
+						
+			}
+		
+	}
